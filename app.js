32619f568efadef5f545bd25130bc3a1aa3e9ca5
miuser <miuser@email.com>
2024-07-24 23:18:38 +0800

    完善登录逻辑

--- app.js
+++ app.js
@@ -2,21 +2,34 @@ const request = require('./pages/utils/request.js')
 
 App({
   onShow(options) {
-    // this.login();
+    this.login();
   },
 
   login() {
     var token = wx.getStorageSync('token')
-    if(!token){
+    // 校验token，如果没有，执行登录
+    if (!token) {
       this.wechatLogin()
+    } else {
+      // 如果有token，请求服务器校验
+      request.get('/user/getUserByToken?token=' + token)
+      .then(response => {
+        if(response.code != 0){
+          this.wechatLogin()
+        }
+      })
     }
   },
 
   wechatLogin(){
     wx.login({
       success: (res) => {
-      
-      },
+        request.get('/user/login?code=' + res.code)
+        .then(response => {
+          var token = response.data.token;
+          wx.setStorageSync('token', token)
+        })
+      }
     })
   }
 
--- pages/utils/request.js
+++ pages/utils/request.js
@@ -1,44 +1,41 @@
-// 发送 GET 请求
-function get(url) {
-  return request('GET', url);
-}
+const request = {
+  get(url) {
+    return this._request('GET', url, {});
+  },
 
-// 发送 POST 请求
-function post(url, data = {}) {
-  return request('POST', url, data);
-}
-
-// 发送请求的通用函数
-function request(method, url, data = {}) {
-  return new Promise((resolve, reject) => {
-    wx.request({
-      url: getApiUrl() + url,
-      method: method,
-      data: data,
-      header: {
-        'token': wx.getStorageSync('token')
-      },
-      success: (res) => {
-        resolve(res.data);
-      },
-      fail: (err) => {
-        reject(err);
-      }
+  post(url, data = {}) {
+    return this._request('POST', url, data);
+  },
+  
+  _request(method, url, data = {}) {
+    return new Promise((resolve, reject) => {
+      wx.request({
+        url: getApiUrl() + url,
+        method: method,
+        data: data,
+        header: {
+          'token': wx.getStorageSync('token')
+        },
+        success: (res) => {
+          resolve(res.data);
+        },
+        fail: (err) => {
+          reject(err);
+        }
+      });
     });
-  });
-}
+  }
+};
 
-// 根据小程序的 `wx.getAccountInfoSync()` 获取环境
-// https://developers.weixin.qq.com/miniprogram/dev/api/open-api/account-info/wx.getAccountInfoSync.html
 function getApiUrl() {
   let envVersion = wx.getAccountInfoSync().miniProgram.envVersion;
   if (envVersion == 'develop') {
     return 'http://localhost:5039';
   } else if (envVersion == 'trial') {
     return 'https://ai-tools.gpt6.plus';
-  } else{
+  } else {
     return 'https://ai-tools.gpt6.plus';
   }
 }
 
-module.exports = { get, post };
+module.exports = request;
